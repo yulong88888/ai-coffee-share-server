38912b7523ba05c05a1510ca1330bbccc8efc111
yulong <1327417509@qq.com>
2018-04-17 10:01:11 +0800

    Initial commit

---
+++ src/main/java/net/lengmang/aicoffeeshareserver/AiCoffeeShareServerApplication.java
@@ -0,0 +1,12 @@
+package net.lengmang.aicoffeeshareserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoffeeShareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCoffeeShareServerApplication.class, args);
+    }
+}
---
+++ src/test/java/net/lengmang/aicoffeeshareserver/AiCoffeeShareServerApplicationTests.java
@@ -0,0 +1,16 @@
+package net.lengmang.aicoffeeshareserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AiCoffeeShareServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
